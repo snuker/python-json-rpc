849fd7575707a4956c66441851ee0ad00f5756be
snuker <nextdmitriy@gmail.com>
2017-05-22 01:49:55 +0300

    Added basic web auth support

--- jsonrpc/proxy.py
+++ jsonrpc/proxy.py
@@ -1,5 +1,6 @@
 
 """
+  Copyright (c) 2017 Suhanov Dmitriy
   Copyright (c) 2007 Jan-Klaas Kollhof
 
   This file is part of jsonrpc.
@@ -19,7 +20,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-import urllib
+import urllib2, base64
 from jsonrpc.json import dumps, loads
 
 class JSONRPCException(Exception):
@@ -28,18 +29,26 @@ class JSONRPCException(Exception):
         self.error = rpcError
         
 class ServiceProxy(object):
-    def __init__(self, serviceURL, serviceName=None):
+    def __init__(self, serviceURL, serviceName=None,auth=None):
         self.__serviceURL = serviceURL
         self.__serviceName = serviceName
+	self.__auth=auth
 
     def __getattr__(self, name):
         if self.__serviceName != None:
             name = "%s.%s" % (self.__serviceName, name)
-        return ServiceProxy(self.__serviceURL, name)
+        return ServiceProxy(self.__serviceURL, name,self.__auth)
 
     def __call__(self, *args):
+         print self.__auth
          postdata = dumps({"method": self.__serviceName, 'params': args, 'id':'jsonrpc'})
-         respdata = urllib.urlopen(self.__serviceURL, postdata).read()
+         request = urllib2.Request(self.__serviceURL)
+         if self.__auth:
+             login,passwd=self.__auth
+             base64string = base64.encodestring('%s:%s' % (login, passwd)).replace('\n', '')
+             request.add_header("Authorization", "Basic %s" % base64string)   
+         request.add_data(postdata)
+         respdata = urllib2.urlopen(request).read()
          resp = loads(respdata)
          if resp['error'] != None:
              raise JSONRPCException(resp['error'])
